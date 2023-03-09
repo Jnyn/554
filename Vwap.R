5955c3bc5673bd9e73a68bee78482aa75c538c42
Jnyn <111448131+Jnyn@users.noreply.github.com>
2023-03-09 17:38:12 -0600

    Add files via upload

--- Vwap.R
+++ Vwap.R
@@ -5,6 +5,7 @@ library(quantmod) #for getSymbols
 library(PerformanceAnalytics)
 library(forecast)
 library(quantstrat)
+library(TTR)
 # Create initdate, from, and to strings
 initdate <- "1999-01-01"
 from <- "2003-01-01"
@@ -19,7 +20,7 @@ Sys.setenv(TZ="UTC")
 currency("USD")
 #Treat as basic equity
 stock("SPY",currency = "USD")
-# Define your trade size and initial equity
+# Define your trade size and initial equity----------
 tradesize <- 100000
 initeq <- 100000
 # Define the names of your strategy, portfolio and account
@@ -38,7 +39,111 @@ initAcct(account.st, portfolios = portfolio.st, initDate = initdate, currency = 
 initOrders(portfolio.st, initDate = initdate)
 # Store the strategy
 strategy(strategy.st, store = TRUE)
+#Create Indicator for VWAP trading*********--------- 
 
-#Create Indicator for VWAP trading--------- 
+#opening_price need to add here*****---------
+
+
+#need to calculate VWAP---------
+VWAP <- sum(SPY$SPY.Adjusted * SPY$SPY.Volume) / sum(SPY$SPY.Volume)
 #Create Signal > use SigThreshold for VWAP---------
-#Entry above Vwap
+#entry_short opening price above Vwap
+
+add.signal(strategy.st, name = "sigThreshold", 
+           
+           # compare VWAP
+           arguments = list(column = "opening_price", 
+                            
+                            # The threshold is v
+                            threshold = VWAP, 
+                            
+                            # We want the oscillator to be over this value
+                            relationship = "gt", 
+                            
+                            # We are interested only in the cross
+                            cross = TRUE), 
+           
+           # Label it shortthreshold
+           label = "entry_short")
+#entry_long opening price below Vwap
+add.signal(strategy.st, name = "sigThreshold", 
+           
+           # compare VWAP
+           arguments = list(column = "opening_price", 
+                            
+                            # The threshold is v
+                            threshold = VWAP, 
+                            
+                            # We want the oscillator to be over this value
+                            relationship = "lt", 
+                            
+                            # We are interested only in the cross
+                            cross = TRUE), 
+           
+           # Label it longthreshold
+           label = "entry_long")
+#RULE--------------
+#make trade when over VWAP
+# Create an entry rule of 1 share when all conditions line up to enter into a position
+#entry_short
+add.rule(strategy.st, name = "ruleSignal", 
+         
+         # Use the shortentry column as the sigcol
+         arguments=list(sigcol = "entry_short", 
+                        
+                        # Set sigval to TRUE
+                        sigval = TRUE, 
+                        
+                        # Set orderqty to 1
+                        orderqty = 1,
+                        
+                        # Use a market type of order
+                        ordertype = "market",
+                        
+                        # Take the long orderside
+                        orderside = "short",
+                        
+                        # Do not replace other signals
+                        replace = FALSE, 
+                        
+                        # Buy at the next day's opening price
+                        prefer = "Open"),
+         
+         # This is an enter type rule, not an exit
+         type = "enter")
+#entry_long
+add.rule(strategy.st, name = "ruleSignal", 
+         
+         # Use the shortentry column as the sigcol
+         arguments=list(sigcol = "entry_long", 
+                        
+                        # Set sigval to TRUE
+                        sigval = TRUE, 
+                        
+                        # Set orderqty to 1
+                        orderqty = 1,
+                        
+                        # Use a market type of order
+                        ordertype = "market",
+                        
+                        # Take the long orderside
+                        orderside = "long",
+                        
+                        # Do not replace other signals
+                        replace = FALSE, 
+                        
+                        # Buy at the next day's opening price
+                        prefer = "Open"),
+         
+         # This is an enter type rule, not an exit
+         type = "enter")
+#building strategy--------------
+# Use applyStrategy() to apply your strategy. Save this to out
+out <- applyStrategy(strategy = strategy.st, portfolios = portfolio.st)
+# Update your portfolio (portfolio.st)
+updatePortf(portfolio.st)
+daterange <- time(getPortfolio(portfolio.st)$summary)[-1]
+
+# Update your account (account.st)
+updateAcct(account.st, daterange)
+updateEndEq(account.st)
