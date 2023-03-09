0527135ba6e17481f84d2b43ab0f80bcdafcd453
Jnyn <111448131+Jnyn@users.noreply.github.com>
2023-03-09 16:26:04 -0600

    Add files via upload

--- GARCH.R
+++ GARCH.R
@@ -2,6 +2,7 @@
 library(rugarch)
 library(quantmod)
 library(PerformanceAnalytics)
+library(forecast)
 # Load the data
 getSymbols("SPY",
            from = "2011-01-01",
@@ -10,6 +11,8 @@ getSymbols("SPY",
            adjust = TRUE)
 return = CalculateReturns(SPY$SPY.Adjusted)
 data = return[-c(1),]
-spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)), mean.model = list(armaOrder = c(0, 0)))
+arima_model <- auto.arima(data)
+mean_spec <- list(armaOrder = c(arima_model$arma[1], arima_model$arma[2]))
+spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)), mean.model = mean_spec)
 fit <- ugarchfit(spec = spec, data = data, solver = "solnp")
-vol<-sigma(fit)
+vol <- as.numeric(sigma(fit)[1, ])
